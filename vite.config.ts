968f5c2084694b14235a226f49b255ad44e5c76b
TheMordegod <levysilva72@gmail.com>
2022-08-23 20:05:41 -0300

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/To-do-List/',
+  base: 'https://themordegod.github.io/To-do-List/',
   plugins: [react()]
 })
